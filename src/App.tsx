91f9c3178a81674139ee31990573cb719d97d8c6
khian calderon <ckhianvictory@gmail.com>
2025-10-22 09:00:52 +0800

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { NavBar } from "./components/navbar";
+
 export default function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <>
+
+      <NavBar 
+        className="bg-purple-800 text-white shadow-lg"
+        image="icons/exponentium-construction.png"
+        title="Exponentium"
+        buttonsAlignment="right"
+        buttons={[
+          {
+            label: "Home",
+            action: () => alert("Clicked Home"),
+            className: "hover:bg-gray-700 px-5 py-2 font-medium rounded-lg"
+          },
+          {
+            label: "Shop",
+            action: () => alert("Clicked Shop"),
+            className: "hover:bg-gray-700 px-5 py-2 font-medium rounded-lg"
+          },
+          {
+            label: "Contact",
+            action: () => alert("Clicked Contact"),
+            className: "hover:bg-gray-700 px-5 py-2 font-medium rounded-lg"
+          }
+        ]}
+      />
+
+    </>
   )
 }
