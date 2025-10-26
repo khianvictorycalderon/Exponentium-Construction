2b6aae4ff4675b8f69854bb23cd0431b0c30aab2
khian calderon <ckhianvictory@gmail.com>
2025-10-26 12:59:29 +0800

    Added line breaks

--- src/sections/lorem.tsx
+++ src/sections/lorem.tsx
@@ -1,5 +1,10 @@
 export const LoremIpsum = () => (
     <>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <p>
         Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
       </p>
