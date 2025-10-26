2490b06d72fc6495ac8fe19a8e297e8792c1f92c
khian calderon <ckhianvictory@gmail.com>
2025-10-26 11:10:34 +0800

    Completed certificates section

---
+++ src/components/certificate-card.tsx
@@ -0,0 +1,21 @@
+interface CertificateCardProps {
+    key: string;
+    image: string;
+}
+
+export const CertificateCard = ({
+    key,
+    image,
+}: CertificateCardProps) => (
+    <div 
+        key={key}
+        className="flex-1/2 md:flex-1/4"
+        >
+        <a href={image} target="_blank">
+            <img 
+                className="w-full h-auto cursor-pointer hover:brightness-110 hover:scale-110 transition duration-300"
+                src={image}
+            />
+        </a>
+    </div>
+)
--- src/sections/certificates.tsx
+++ src/sections/certificates.tsx
@@ -1,14 +1,41 @@
+import { CertificateCard } from "../components/certificate-card";
 import SectionContainer from "../components/section-container"
 
 export const CertificatesSection = () => {
+
+    const Certificates = [
+        {
+            image: "images/certificates/iso.png"
+        },
+        {
+            image: "images/certificates/dole.png"
+        },
+        {
+            image: "images/certificates/ciap.png"
+        },
+        {
+            image: "images/certificates/philgeps.png"
+        },
+    ]
+
     return (
         <section className="pt-8">
             <SectionContainer className="text-center">
                 <>
                     <h2 className="text-2xl md:text-3xl lg:text-4xl text-orange-500 font-bold">Certification & Accreditation</h2>
                     <h3 className="text-base md:text-lg lg:text-xl italic text-gray-700 font-semibold">We meet the highest industry standards and maintain full compliance with regulatory bodies.</h3>
+
+                    <div className="flex flex-wrap pt-8">
+                        {Certificates.map((item, index) => (
+                            <CertificateCard
+                                key={`${item.image}-${index}`}
+                                image={item.image}
+                            />
+                        ))}
+                    </div>
+
                 </>
             </SectionContainer>
         </section>
-    )
+    );
 }
