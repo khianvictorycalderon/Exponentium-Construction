21b59e93afd8dbdea0c032848397f51515723fc3
khian calderon <ckhianvictory@gmail.com>
2025-10-26 11:22:12 +0800

    Updated to responsive padding top

--- src/sections/certificates.tsx
+++ src/sections/certificates.tsx
@@ -19,13 +19,13 @@ export const CertificatesSection = () => {
     ]
 
     return (
-        <section className="pt-8">
+        <section className="pt-8 md:pt-16 lg:pt-24">
             <SectionContainer className="text-center">
                 <>
                     <h2 className="text-2xl md:text-3xl lg:text-4xl text-orange-500 font-bold">Certification & Accreditation</h2>
                     <h3 className="text-base md:text-lg lg:text-xl italic text-gray-700 font-semibold">We meet the highest industry standards and maintain full compliance with regulatory bodies.</h3>
 
-                    <div className="flex flex-wrap pt-8">
+                    <div className="flex flex-wrap pt-8 md:pt-16 lg:pt-24">
                         {Certificates.map((item, index) => (
                             <CertificateCard
                                 key={`${item.image}-${index}`}
